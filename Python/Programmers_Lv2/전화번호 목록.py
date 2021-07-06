e50a25ef15dbcf6c49b490c99cb76adebc397508
DESKTOP-89L3KMD\Kanghy <gmldus0814@naver.com>
2021-07-06 11:25:42 +0900

    20210706

---
+++ Python/Programmers_Lv2/전화번호 목록.py
@@ -0,0 +1,6 @@
+def solution(phone_book):
+    phone_book.sort()
+    for phone1, phone2 in zip(phone_book, phone_book[1:]):
+        if phone2.startswith(phone1):
+            return False
+    return True
