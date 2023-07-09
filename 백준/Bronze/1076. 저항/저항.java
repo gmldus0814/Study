439826bb59fdb42273b5bf23827610802dba84e6
gmldus0814 <69186264+gmldus0814@users.noreply.github.com>
2023-07-09 15:04:23 +0900

    [Bronze II] Title: 저항, Time: 212 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Bronze/1076. 저항/저항.java
+++ 백준/Bronze/1076. 저항/저항.java
@@ -15,23 +15,18 @@ public class Main {
 		
 		
 		Scanner sc = new Scanner(System.in);
-		int colorInput1 = colorArr.indexOf(sc.next()); //첫번째 color
+		int colorInput1 = colorArr.indexOf(sc.next()) * 10; //첫번째 color
 		int colorInput2 = colorArr.indexOf(sc.next()); //두번째 color
 		int colorInput3 = colorArr.indexOf(sc.next()); //세번째 color
-		
-		
-		//인덱스 번호를 String으로 형변환 해준다.
-		String colorResult1 = Integer.toString(colorInput1); 
-		String colorResult2 = Integer.toString(colorInput2);
-		
-		
-		//black이 들어왔을 때 -1이 되는 것을 방지하기 위해 double로 받아준다.
-		double result2 = Integer.parseInt(colorResult1 + colorResult2);
+
 		
 		//인덱스번호+인덱스번호 (4+7 = 47) * 10에 마지막 color의 인덱스 번호대로 제곱해주는 식
-		long result1 = (int)Math.pow(10,colorInput3); //정수의 길이가 길어지므로 long으로 받아준다.
-
-		System.out.println((long)result2 * result1);
+		long result1 = (long) Math.pow(10,colorInput3); //정수의 길이가 길어지므로 long으로 받아준다.
+		
+		//black이 들어왔을 때 -1이 되는 것을 방지하기 위해 double로 받아준다.
+		int result2 = colorInput1 + colorInput2;
+		
+		System.out.println((long)(result1 * result2));
 		
 		
 	}
