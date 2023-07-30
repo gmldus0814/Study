c6e41a272c72054615a25d48d2b202c820f68970
gmldus0814 <69186264+gmldus0814@users.noreply.github.com>
2023-07-30 17:44:46 +0900

    [Silver II] Title: 나무 자르기, Time: 1800 ms, Memory: 321212 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,70 +1,66 @@
 import java.util.Scanner;
- 
+
 public class Main {
+	
+	/*
+	 * M미터의 나무가 필요
+	 * 높이 H지정하면 톱날이 땅으로부터 H미터 위로 올라감
+	 * 한줄에 연속해 있는 나무를 모두 절단
+	 * 높이가 H보다 큰 나무는 H위의 부분이 잘릴 것임
+	 */
+	
 	public static void main(String[] args) {
 		
-		Scanner in = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
-		int N = in.nextInt();
-		int M = in.nextInt();
+		int num = sc.nextInt();
+		int M = sc.nextInt();
 		
-		int[] tree = new int[N];
+		int tree[] = new int[num];
 		
-		int min = 0;
 		int max = 0;
-		
-		for(int i = 0; i < N; i++) {
-			tree[i] = in.nextInt();
-			
-			/*
-			 * 나무들 중 최댓값을 구하기 위해 매 입력 때마다 max 변수와 비교하여
-			 * 입력 받은 나무가 max보다 클 경우 max 값을 해당 나무의 높이로 갱신해준다. 
-			 */
-			if(max < tree[i]) {
+		int min = 0;
+
+		for(int i=0; i<num; i++) {
+			tree[i] = sc.nextInt();
+			//최댓값 구하기
+			if(tree[i] > max) {
 				max = tree[i];
 			}
 		}
 		
-		// 이분 탐색 (upper bound)
+		//min값이 max보다 작으면 while문 break;
+		//이진탐색
 		while(min < max) {
 			
-			int mid = (min + max) / 2;
-			long sum = 0;
-			for(int treeHeight : tree) {
+			
+			//max의 중간값
+			int mid = (min+max)/2;
+			long result = 0;
+			
+			
+			for(int t:tree) {
 				
-				/*
-				 *  tree의 잘린 길이 = tree의 높이 - 자르는 위치(mid)
-				 *  tree의 잘린 길의의 합을 구한다.
-				 *  이 때 자르는 위치가 주어진 나무의 높이보다 클 수 있기 때문에
-				 *  0 이하인 경우(=음수)에는 합산을 하지 않고 양수만 합산하도록 해야한다.
-				 */
-				if(treeHeight - mid > 0) { 
-					sum += (treeHeight - mid);
+				//tree의 잘린 길이의 합
+				//양수만 합칠 수 있게 조건문을 달아둠
+				if(t - mid > 0) {
+					result += (t-mid);
 				}
 			}
 			
- 
-			/*
-			 * 자른 나무 길의의 합이 M보다 작다는 것은
-			 * 자르는 위치(높이)가 높다는 의미이므로 높이를 낮춰야 한다.
-			 * 즉, 상한(max)를 줄여야 한다.
-			 */
-			if(sum < M) {
+			//만약 result가 M보다 작으면 자르는 위치가 높기 때문에 높이를 낮춰야 함
+			if(result < M) {
 				max = mid;
 			}
 			
-			/*
-			 * 자른 나무 길이의 합이 M보다 크다는 것은
-			 * 자르는 위치(높이)가 낮다는 의미이므로 높이를 높여야 한다.
-			 * 또한, 같을 경우에는 최대한 적게 자르기 위해 자르는 높이를
-			 * 높여야 하므로 하한(min)을 올려야 한다.
-			 */
+			//만약 result가 M보다 크면 자르는 위치가 낮기 때문에 높이를 높여야 함
 			else {
-				min = mid + 1;
+				min = mid+1;
 			}
+			
 		}
-		
-		System.out.println(min - 1);
-		
+
+		System.out.println(min-1);
 	}
+
 }
